31613cd78fd2ef2ac49b21093b48d6e37a783fea
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-27 11:23:37 +0200

    MongoDB Service Configuration, Connection and deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,40 @@
 require("dotenv").config();
 import logger from "morgan";
+import mongoose from "mongoose";
 import express from "express";
 import routes from "./routes";
 import { errorHandler } from "./middlewares";
+
+const start = async () => {
+  mongoose.Promise = Promise;
+  process.env.NODE_ENV === "development" ? mongoose.set("debug", true) : null;
+
+  const URI = process.env.MONGO_DB_URI
+    ? process.env.MONGO_DB_URI
+    : "auth-mongo-service";
+
+  await mongoose
+    .connect(`mongodb://${URI}:27017/auth`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log(`Connected to ${URI} Database Successfully`);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .then(() => {
+      const PORT =
+        process.env.PORT && parseInt(process.env.PORT)
+          ? parseInt(process.env.PORT)
+          : 3000;
+      server.listen(PORT, () => {
+        console.log(`Auth Service is running on port: ${PORT}`);
+      });
+    });
+};
 
 const server = express();
 
@@ -38,10 +70,4 @@ server.use(errorHandler);
  * Start Server
  *
  */
-const PORT =
-  process.env.PORT && parseInt(process.env.PORT)
-    ? parseInt(process.env.PORT)
-    : 3000;
-server.listen(PORT, () => {
-  console.log(`Auth Service is running on port: ${PORT}`);
-});
+start();
