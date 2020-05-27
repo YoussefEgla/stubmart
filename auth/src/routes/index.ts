134750e641f82c584e6f08737cd848fe83677175
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-27 09:56:43 +0200

    Handle 404

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -5,6 +5,7 @@ import currentUser from "./current-user";
 import login from "./login";
 import logout from "./logout";
 import signup from "./signup";
+import { Error404 } from "../errors";
 
 const server = express();
 
@@ -12,5 +13,14 @@ server.use("/api/auth/", signup);
 server.use("/api/auth/", login);
 server.use("/api/auth/", logout);
 server.use("/api/auth/", currentUser);
+
+/**
+ *
+ * Handle 404
+ *
+ */
+server.all("*", (req, res, next) => {
+  next(new Error404());
+});
 
 export default server;
